7234ad7bea1e97876530ec0445c5009b8f2d5619
vyaaki1994 <vyaaki1994@gmail.com>
2020-05-21 17:59:08 +0300

    Scene changing (ship + background + camera + light). Add borders for scene, tilt for ship on moving.

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    private Rigidbody playerShip;
+    [SerializeField] private float speed = 20;
+    [SerializeField] private float xMinBorder;
+    [SerializeField] private float xMaxBorder;
+    [SerializeField] private float zMinBorder;
+    [SerializeField] private float zMaxBorder;
+    [SerializeField] private float tilt;
+
+    void Start()
+    {
+        playerShip = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        
+        playerShip.velocity = new Vector3(moveHorizontal,0,moveVertical)* speed;
+
+        float restrictedX = Mathf.Clamp(playerShip.position.x, xMinBorder, xMaxBorder);
+        float restrictedZ = Mathf.Clamp(playerShip.position.z, zMinBorder, zMaxBorder);
+
+        playerShip.position = new Vector3(restrictedX, 0 , restrictedZ);
+
+        var velocity = playerShip.velocity;
+        playerShip.rotation = Quaternion.Euler(tilt * velocity.z, 0, -velocity.x * tilt);
+    }
+}
