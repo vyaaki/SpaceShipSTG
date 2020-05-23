ced05838d3956d4dde33ae28e5035c602fd0fd29
vyaaki1994 <vyaaki1994@gmail.com>
2020-05-23 17:47:26 +0300

    add small lazer guns.

--- Assets/Scripts/LazerScript.cs
+++ Assets/Scripts/LazerScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class LazerScript : MonoBehaviour
 {
-    [SerializeField] private float speed;
+    public float speed;
     void Start()
     {
         GetComponent<Rigidbody>().velocity= new Vector3(0,  0, speed);
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -14,13 +14,20 @@ public class PlayerScript : MonoBehaviour
     [SerializeField] private float tilt;
 
     [SerializeField] private GameObject lazerShot;
+    [SerializeField] private GameObject smallLazerShot;
     [SerializeField] private Transform lazerGun;
+    [SerializeField] private List<Transform> smallLazers;
     [SerializeField] private float shotDelay;
-    private float nextShotTime;
     
+    [SerializeField] float lazerSpeed;
+    private float nextShotTime, smallShotDelay, nextSmallShotTime;
+
+    private const int SMALL_LAZER_DIVIDER = 4;
+    private const int SMALL_LAZER_DELAY_DIVIDER = 2;
     void Start()
     {
         playerShip = GetComponent<Rigidbody>();
+        smallShotDelay = shotDelay / SMALL_LAZER_DELAY_DIVIDER;
     }
 
     void Update()
@@ -40,8 +47,24 @@ public class PlayerScript : MonoBehaviour
 
         if (Time.time > nextShotTime && Input.GetButton(UnityConstants.Axes.Fire1))
         {
-            Instantiate(lazerShot, lazerGun.position, Quaternion.identity);
+            GameObject lazer = Instantiate(lazerShot, lazerGun.position, Quaternion.identity);
+            lazer.GetComponent<Rigidbody>().velocity= new Vector3(0,  0, lazerSpeed);
             nextShotTime = Time.time + shotDelay;
+        }
+
+        if (Time.time > nextSmallShotTime && Input.GetButton(UnityConstants.Axes.Fire2))
+        {
+            int isRight = -1;
+            foreach (Transform lazer in smallLazers)
+            {
+                GameObject smallLazer = Instantiate(smallLazerShot, lazer.position, Quaternion.Euler(0, 45, 0));
+                smallLazer.transform.rotation = Quaternion.Euler(0, 45, 0);
+                smallLazer.GetComponent<Rigidbody>().velocity = new Vector3(lazerSpeed * isRight, 0 ,lazerSpeed);
+                smallLazer.transform.localScale /= SMALL_LAZER_DIVIDER;
+                isRight = 1;
+            }
+            nextSmallShotTime = Time.time + smallShotDelay;
+            
         }
     }
 }
